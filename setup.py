09e0e63f8b18425c25923f93a8d252258a5d93ec
Dhinesh Raja <dhineshraja@live.com>
2022-11-28 13:06:19 +0530

    updated on 28/11

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+
+def get_requirements()-> List[str]:
+    """
+    This function will return the list of requirements to be installed 
+    """
+    requirement_list=[]
+    return requirement_list
+
+
+
+setup(
+
+name="sensor",
+version="1.1.0",
+author="Dhinesh Raja",
+author_email="dhineshraja@live.com",
+packages=find_packages(),
+install_requires=[],
+
+)
